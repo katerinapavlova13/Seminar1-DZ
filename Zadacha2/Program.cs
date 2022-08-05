1171f592e8945823d62f5112c6b87d53c71a6c39
Katerina Pavlova <malinakate18@gmail.com>
2022-08-05 18:09:09 +0400

    Zadacha2+

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -3,3 +3,21 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+        Console.Write("Введите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите третье число: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        if (a > b && a > c)
+{
+            Console.WriteLine($"{a} = max");
+        }
+else if (b > a && b > c)
+{
+            Console.WriteLine($"{b} = max");
+        }
+else if (c > a && c > b)
+{
+            Console.WriteLine($"{c} = max");
+        }
